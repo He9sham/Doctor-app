d7fabda4d09b3101fbb0cdef42ea414b5d79cc3c
He9sham <heshamhamdan51@gmail.com>
2024-05-16 16:40:17 +0300

    create a api service

---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:doctor/core/networking/api_constanse.dart';
+import 'package:doctor/features/auth_features/data/models/login_request_body.dart';
+import 'package:doctor/features/auth_features/data/models/login_response.dart';
+import 'package:retrofit/http.dart';
+part 'api_service.g.dart';
+@RestApi(baseUrl: ApiConstaens.apiBaseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(ApiConstaens.login)
+  Future<LoginResponse> login(
+    @Body() LoginRequestBody loginRequestBody,
+  );
+}
