830bbfd91763f4ade04de9989bc954bdd140ea68
He9sham <heshamhamdan51@gmail.com>
2024-05-17 22:26:35 +0300

    i need refactor this shit

--- lib/features/auth_features/view/widgets/InputUserEmailAndPassAndConfirmForSighIn.dart
+++ lib/features/auth_features/view/widgets/InputUserEmailAndPassAndConfirmForSighIn.dart
@@ -5,6 +5,7 @@ import 'package:doctor/features/auth_features/view/widgets/Custom_Text_felid.dar
 import 'package:doctor/features/auth_features/view/widgets/buttom_navigate_row.dart';
 import 'package:doctor/features/auth_features/view/widgets/custom_container_auth.dart';
 import 'package:doctor/features/auth_features/view/widgets/custom_container_auth_Input_blue.dart';
+import 'package:doctor/features/auth_features/view/widgets/login_bloc_listener.dart';
 import 'package:doctor/features/auth_features/view/widgets/password_validations.dart';
 import 'package:doctor/features/auth_features/view_models/login_cubit/login_cubit.dart';
 import 'package:flutter/material.dart';
@@ -33,91 +34,94 @@ class _InputUserEmailAndPassAndConfirmForSighInState
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: MediaQuery.sizeOf(context).width * 0.07,
-      child: CustomContainerAuthInputBlue(
-        height: MediaQuery.sizeOf(context).height.h * 0.8,
-        child: Form(
-          key: context.read<LoginCubit>().formkey,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                Image.asset(klogoapp),
-                const SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  'Markaz ElAmal',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontFamily: GoogleFonts.peralta().fontFamily,
+    return Builder(builder: (context) {
+      return Positioned(
+        left: MediaQuery.sizeOf(context).width * 0.07,
+        child: CustomContainerAuthInputBlue(
+          height: MediaQuery.sizeOf(context).height.h * 0.8,
+          child: Form(
+            key: context.read<LoginCubit>().formkey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Image.asset(klogoapp),
+                  const SizedBox(
+                    height: 15,
                   ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                CustomTextfelid(
-                  obscureText: false,
-                  controller: context.read<LoginCubit>().emailController,
-                  onChanged: (data) {},
-                  validator: (data) {
-                    if (data == null || data.isEmpty) {
-                      return 'please enter a valid email';
-                    }
-                    return null;
-                  },
-                  title: 'Email',
-                  iconData: Icons.email_outlined,
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                CustomTextfelid(
-                  obscureText: true,
-                  controller: context.read<LoginCubit>().passwordController,
-                  onChanged: (data) {},
-                  validator: (data) {
-                    if (data == null || data.isEmpty) {
-                      return 'please enter a valid password';
-                    }
-                    return null;
-                  },
-                  title: 'password',
-                  iconData: Icons.lock,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                CustomContenerauth(
-                  ontap: () {
-                    {
-                      validateThePassword(context);
-                    }
-                  },
-                  title: 'Sign in',
-                ),
-                ButtomNavigateRow(
-                  onpressed: () {
-                    GoRouter.of(context).push(AppRouter.sighupview);
-                  },
-                  title: 'Don’t have an account?',
-                  typeauth: 'Sign up',
-                ),
-                PasswordValidations(
-                  hasLowerCase: hasLowercase,
-                  hasUpperCase: hasUpperCase,
-                  hasSpecialCharacters: hasSpecialCharacters,
-                  hasNumber: hasNumber,
-                  hasMinLength: hasMinLength,
-                )
-              ],
+                  Text(
+                    'Markaz ElAmal',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: GoogleFonts.peralta().fontFamily,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  CustomTextfelid(
+                    obscureText: false,
+                    controller: context.read<LoginCubit>().emailController,
+                    onChanged: (data) {},
+                    validator: (data) {
+                      if (data == null || data.isEmpty) {
+                        return 'please enter a valid email';
+                      }
+                      return null;
+                    },
+                    title: 'Email',
+                    iconData: Icons.email_outlined,
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  CustomTextfelid(
+                    obscureText: true,
+                    controller: context.read<LoginCubit>().passwordController,
+                    onChanged: (data) {},
+                    validator: (data) {
+                      if (data == null || data.isEmpty) {
+                        return 'please enter a valid password';
+                      }
+                      return null;
+                    },
+                    title: 'password',
+                    iconData: Icons.lock,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  CustomContenerauth(
+                    ontap: () {
+                      {
+                        validateThePassword(context);
+                      }
+                    },
+                    title: 'Sign in',
+                  ),
+                  ButtomNavigateRow(
+                    onpressed: () {
+                      GoRouter.of(context).push(AppRouter.sighupview);
+                    },
+                    title: 'Don’t have an account?',
+                    typeauth: 'Sign up',
+                  ),
+                  PasswordValidations(
+                    hasLowerCase: hasLowercase,
+                    hasUpperCase: hasUpperCase,
+                    hasSpecialCharacters: hasSpecialCharacters,
+                    hasNumber: hasNumber,
+                    hasMinLength: hasMinLength,
+                  ),
+                  const LoginBlocListener(),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 
   void validateThePassword(BuildContext context) {
---
+++ lib/features/auth_features/view/widgets/login_bloc_listener.dart
@@ -0,0 +1,66 @@
+import 'package:doctor/core/theming/styles.dart';
+import 'package:doctor/core/utils/app_router.dart';
+import 'package:doctor/features/auth_features/view_models/login_cubit/login_cubit.dart';
+import 'package:doctor/features/auth_features/view_models/login_cubit/login_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+class LoginBlocListener extends StatelessWidget {
+  const LoginBlocListener({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<LoginCubit, LoginState>(
+      listenWhen: (previous, current) =>
+          current is Loading || current is Success || current is Error,
+      listener: (context, state) {
+        state.whenOrNull(
+          loading: () {
+            showDialog(
+              context: context,
+              builder: (context) => const Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          },
+          success: (loginResposn) {
+            context.pop();
+            GoRouter.of(context).push(AppRouter.welcomeview);
+          },
+          error: (error) {
+            setupErrorState(context, error);
+          },
+        );
+      },
+    );
+  }
+
+  void setupErrorState(BuildContext context, String error) {
+    context.pop();
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        icon: const Icon(
+          Icons.error,
+          color: Colors.red,
+          size: 32,
+        ),
+        content: Text(
+          error,
+          style: Styles.textmid,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              context.pop();
+            },
+            child: const Text(
+              'Got it',
+              style: Styles.textmid,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
