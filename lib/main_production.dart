99db35bb689b2cf045dc9afed2c31c89daa4951d
He9sham <heshamhamdan51@gmail.com>
2024-05-28 22:25:47 +0300

    add flavors to app (development & production) add lunch.json delete main file and add main develoment and production.

---
+++ lib/main_production.dart
@@ -0,0 +1,10 @@
+import 'package:doctor/core/di/dependency_injection.dart';
+import 'package:doctor/docapp.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  await ScreenUtil.ensureScreenSize();
+  setupGetIt();
+  runApp(const DoctorApp());
+}
