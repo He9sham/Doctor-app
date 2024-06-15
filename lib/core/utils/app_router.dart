4d7eaf898def47447ddee49f0546bbc4336f4671
He9sham <heshamhamdan51@gmail.com>
2024-06-16 02:12:56 +0300

    An attempt to add navigation bar results failed

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -5,6 +5,7 @@ import 'package:doctor/features/home/view/home_page.dart';
 import 'package:doctor/features/home/view/welcome_page.dart';
 import 'package:doctor/features/login/logic/login_cubit.dart';
 import 'package:doctor/features/login/view/sigh_in_view.dart';
+import 'package:doctor/features/navigation_appbar_controlor.dart';
 import 'package:doctor/features/onboarding/view/onboarding_view.dart';
 import 'package:doctor/features/sign_up/view_models/signup_cubit/sign_up_cubit.dart';
 import 'package:doctor/features/sign_up/views/sighup_view.dart';
@@ -15,7 +16,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     //this arguments to be passed in any screen like this ( arguments as ClassName )
-    final arguments = settings.arguments;
+    // final arguments = settings.arguments;
 
     switch (settings.name) {
       case Routes.splashScreen:
@@ -43,6 +44,10 @@ class AppRouter {
             create: (context) => getIt<SignupCubit>(),
             child: const SighupView(),
           ),
+        );
+      case Routes.navigationScreen:
+        return MaterialPageRoute(
+          builder: (_) => const NavigationBarView(),
         );
       case Routes.homeScreen:
         return MaterialPageRoute(
