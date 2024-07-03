453b6868add0d97950fcb8726d7d51f83cc2846b
hesha30m <95132216+He9sham@users.noreply.github.com>
2024-07-04 02:46:37 +0300

    finsh ui

--- lib/features/doc_details/view/doc_details_view.dart
+++ lib/features/doc_details/view/doc_details_view.dart
@@ -1,16 +1,41 @@
+import 'package:doctor/core/helper/spacing.dart';
+import 'package:doctor/core/widgets/app_text_button.dart';
+import 'package:doctor/features/doc_details/view/widgets/about_doctor_details.dart';
+import 'package:doctor/features/doc_details/view/widgets/custom_appbar_for_doc_details.dart';
+import 'package:doctor/features/doc_details/view/widgets/custom_row_Show_of_doc.dart';
 import 'package:flutter/material.dart';
 
 class DocDetailsView extends StatelessWidget {
   const DocDetailsView({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: 
-        [
-          
-
-        ],
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CustomAppbarForDocDetails(),
+              verticalSpace(32),
+              const CustomRowShowOfDoc(),
+              verticalSpace(15),
+              const Divider(),
+              verticalSpace(20),
+              const AboutDoctorDetails(),
+              verticalSpace(80),
+              AppTextButton(
+                buttonText: 'Make An Appointment',
+                onPressed: () {},
+                textStyle: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
