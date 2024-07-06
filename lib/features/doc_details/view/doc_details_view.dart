08c16dcbf2387739144e1372c9b6bb75d9c4baee
hesha30m <95132216+He9sham@users.noreply.github.com>
2024-07-06 20:50:56 +0300

    finsh doc details api call

--- lib/features/doc_details/view/doc_details_view.dart
+++ lib/features/doc_details/view/doc_details_view.dart
@@ -3,10 +3,12 @@ import 'package:doctor/core/widgets/app_text_button.dart';
 import 'package:doctor/features/doc_details/view/widgets/about_doctor_details.dart';
 import 'package:doctor/features/doc_details/view/widgets/custom_appbar_for_doc_details.dart';
 import 'package:doctor/features/doc_details/view/widgets/custom_row_Show_of_doc.dart';
+import 'package:doctor/features/home/data/models/specialization_response_api.dart';
 import 'package:flutter/material.dart';
 
 class DocDetailsView extends StatelessWidget {
-  const DocDetailsView({super.key});
+  const DocDetailsView({super.key, required this.doctors});
+  final Doctors doctors;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,13 +18,19 @@ class DocDetailsView extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const CustomAppbarForDocDetails(),
+              CustomAppbarForDocDetails(
+                doctors: doctors,
+              ),
               verticalSpace(32),
-              const CustomRowShowOfDoc(),
+              CustomRowShowOfDoc(
+                doctors: doctors,
+              ),
               verticalSpace(15),
               const Divider(),
               verticalSpace(20),
-              const AboutDoctorDetails(),
+              AboutDoctorDetails(
+                doctors: doctors,
+              ),
               verticalSpace(80),
               AppTextButton(
                 buttonText: 'Make An Appointment',
